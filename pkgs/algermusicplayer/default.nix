084424fef6a619b7ef8df6adc63acf61649f5ec2
CinQwQeggs01 <3544861138@qq.com>
2025-10-02 17:33:00 +0800

    Update default.nix

--- pkgs/algermusicplayer/default.nix
+++ pkgs/algermusicplayer/default.nix
@@ -1,19 +1,26 @@
 { lib, pkgs, appimageTools, fetchurl, ... }:
 
 let
-  version = "4.9.0"; 
+  version = "4.9.0";
   pname = "AlgerMusicPlayer";
   src = fetchurl {
-    url = https://github.com/algerkong/AlgerMusicPlayer/releases/download/v4.9.0/AlgerMusicPlayer-4.9.0-linux-x86_64.AppImage; 
-    sha256 = sha256:7d8f25a1bb118369c310d60363747cd73276c7416bdbdbc971cade95b7608268; # 替
+    url = "https://github.com/algerkong/AlgerMusicPlayer/releases/download/v${version}/AlgerMusicPlayer-${version}-linux-x86_64.AppImage";
+    sha256 = "7d8f25a1bb118369c310d60363747cd73276c7416bdbdbc971cade95b7608268";
   };
 in
 appimageTools.wrapType2 {
   inherit pname version src;
 
-extraPkgs = pkgs: with pkgs; [
-  libglvnd
-  ffmpeg
-  qt5.qtbase 
-];
-  
+  extraPkgs = pkgs: with pkgs; [
+    libglvnd
+    ffmpeg
+    qt5.qtbase
+  ];
+
+  meta = with lib; {
+    description = "A powerful cross-platform music player based on Electron";
+    homepage = "https://github.com/algerkong/AlgerMusicPlayer";
+    license = licenses.mit;
+    platforms = [ "x86_64-linux" ];
+  };
+} 
