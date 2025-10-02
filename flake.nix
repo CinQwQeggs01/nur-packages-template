311ae102df8f093aaf3cedf83cd52aeed567b590
CinQwQeggs01 <3544861138@qq.com>
2025-10-02 15:42:02 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,25 @@
 {
-  description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+  description = "Alger Music Player package for NUR";
+
+  inputs = {
+   
+    nixpkgs.url = "github:NixOS/nixpkgs/unstable"; 
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in 
+  {
+    
+    packages.${system} = {
+      
+      algerMusicPlayer = pkgs.callPackage ./default.nix {};
     };
+    
+    
+    defaultPackage.${system} = self.packages.${system}.algerMusicPlayer;
+  };
 }
