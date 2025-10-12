c7ef967316e024563468551c01d8438c90e9411d
CinQwQeggs <3544861138@qq.com>
2025-10-12 22:06:05 +0800

    Update default.nix

--- pkgs/go-cqhttp/default.nix
+++ pkgs/go-cqhttp/default.nix
@@ -26,8 +26,12 @@ buildGoModule rec {
   #
   meta = with lib; {
     description = "基于 Mirai/MiraiGo 的 OneBot Golang 原生实现";
-    homepage = "https://github.com/Mrs4s/go-cqhttp";
-    sourceProvenance = pkgs.lib.getSources [ src ];  
+    homepage = "https://github.com/Mrs4s/go-cqhttp";sourceProvenance = [
+      (lib.sourceTypes.fromDerivation {
+        derivation = src;
+        isSource = true;
+      })
+    ];
     maintainers = with lib.maintainers; [ cinqwqeggs ];
     mainProgram = "go-cqhttp";
     license = licenses.agpl3Only; 
